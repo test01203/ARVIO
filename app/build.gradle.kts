30680d8eceb09beadc53bebd60c2c56c9da63177
Arvin <arvin@arflix.local>
2026-06-17 20:34:56 +0200

    Bump Play release version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 35
-        versionCode = 296
+        versionCode = 297
         versionName = "1.9.961"
         buildConfigField("String", "GITHUB_OWNER", "\"ProdigyV21\"")
         buildConfigField("String", "GITHUB_REPO", "\"ARVIO\"")
